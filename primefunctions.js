9f98e2867b6ccf1cf831fb2051a100476bb0b17d
ray01111989 <55462898+ray01111989@users.noreply.github.com>
2019-11-21 22:18:48 -0800

    Finished cumulativeSum.

--- primefunctions.js
+++ primefunctions.js
@@ -28,3 +28,25 @@ function PrimeGen(n) {
 }
 // eslint-disable-next-line no-useless-concat
 console.log(`${'PrimeGen => ' + '['}${PrimeGen(10)}]`);
+// this function will gives a new list of numbers from the list was given and give the cumulative list
+// for the fist list.
+function cumulativeSum(x) {
+  const total = [];
+  // This loop will go through the list and count the elements we have
+  for (let i = 0; i < x.length; i++) {
+    // console.log(i);
+    // This will take the number we found and push them to the array
+    if (i === 0) total.push(x[i]); else {
+      // console.log(x[i]);
+      let sum = 0;
+      // This loop will through the elements and sum elements
+      for (let n = i; n >= 0; n--) sum += x[n];
+      // console.log(sum);
+      total.push(sum);
+    }
+  }
+
+  return total;
+}
+// eslint-disable-next-line no-useless-concat
+console.log(`${'cumulativeSum => ' + '['}${cumulativeSum([1, 2, 3, 4])}]`);
