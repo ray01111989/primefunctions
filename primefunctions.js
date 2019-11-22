afb1073376346cb4fb79e6bd08a686e60c942b31
ray01111989 <55462898+ray01111989@users.noreply.github.com>
2019-11-21 22:45:23 -0800

    update

--- primefunctions.js
+++ primefunctions.js
@@ -11,6 +11,7 @@ function PrimeGen(n) {
   // This loop through the numbers until it reach the n numbers we have
 
   for (let i = 2; i < n; i++) {
+    // eslint-disable-next-line no-shadow
     let isPrime = false;
     // this loop will make sure if the numbers we have is primes
     for (let j = 2; j <= i; j++) {
@@ -50,3 +51,60 @@ function cumulativeSum(x) {
 }
 // eslint-disable-next-line no-useless-concat
 console.log(`${'cumulativeSum => ' + '['}${cumulativeSum([1, 2, 3, 4])}]`);
+// This function goes through the elements of the primes numbers and sum them
+// eslint-disable-next-line camelcase
+function sumoF_Primes(num1) {
+  let sum = 0;
+
+  for (let i = 0; i < num1.length; i++) sum += num1[i];
+
+  return sum;
+}
+// This function goes through the elements and make sure they are primes numbers
+function isPrime(num) {
+  for (let j = 2; j * j <= num; j++) {
+    if (num % j === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+// This function will find all the longest sum of consecutive primes that adds to a prime below n number
+function maxPrimeSum(n) {
+  const List = PrimeGen(n);
+
+  let nMax = 0;
+
+  let lenMax = 0;
+
+  let temp;
+
+  let array = [];
+
+  for (let j = 0; j < n; j++) {
+    for (let i = 0; i < j; i++) {
+      array = List.slice(i, j);
+
+      temp = sumoF_Primes(array);
+
+      if (nMax < temp) {
+        if (isPrime(temp) !== false) {
+          if (temp < n) {
+            if (array.length > lenMax) {
+              lenMax = array.length;
+
+              nMax = temp;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return [nMax, lenMax];
+}
+
+// eslint-disable-next-line no-useless-concat
+console.log(`${'maxPrimeSum => ' + '['}${maxPrimeSum(100)}]`);
+// eslint-disable-next-line no-useless-concat
+console.log(`${'maxPrimeSum => ' + '['}${maxPrimeSum(1000)}]`);
