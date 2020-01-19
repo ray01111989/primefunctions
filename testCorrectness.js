73730aa624e2025422ecf4868bb3fa7b8b9b6596
ray01111989 <55462898+ray01111989@users.noreply.github.com>
2020-01-18 23:12:03 -0800

    Update testCorrectness.js

--- testCorrectness.js
+++ testCorrectness.js
@@ -1,15 +1,12 @@
 /* eslint-env mocha, chai */
-// eslint-disable-next-line no-unused-vars
-/* global chai, primeGen, cumulativeSum, maxPrimeSum */
+/* global chai, PrimeGen, cumulativeSum, maxPrimeSum */
 
 describe('Test For Correctness', function () {
   describe('PrimeGen', function () {
     it('primeGen(10) => [2, 3, 5, 7]', function () {
-      // eslint-disable-next-line no-undef
       chai.expect(PrimeGen(10)).to.have.members([2, 3, 5, 7]);
     });
     it('PrimeGen(20) => [2, 3, 5, 7, 11, 13, 17, 19]', function () {
-      // eslint-disable-next-line no-undef
       chai.expect(PrimeGen(20)).to.have.members([2, 3, 5, 7, 11, 13, 17, 19]);
     });
   });
